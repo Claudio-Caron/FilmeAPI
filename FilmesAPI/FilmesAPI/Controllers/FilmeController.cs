372920fe25ce0c04dd67193a2aeadf5a89447b15
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2025-02-13 10:25:01 -0400

    adicionando progresso inicial do projeto

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,32 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class FilmeController:ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private int id = 0;
+
+  
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmesTrecho([FromQuery]int skip ,[FromQuery]int take)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+    [HttpGet("{id}")]
+    public IActionResult RecuperaPorId(int id)
+    {
+        var filmesR = filmes.FirstOrDefault(x => x.Id == id);
+        if (filmesR is null) return NotFound();
+        return Ok(filmesR);
+    }
+    [HttpPost]
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
+    {
+        filme.Id++;
+        filmes.Add(filme);   
+        return CreatedAtAction(nameof(RecuperaPorId), new { id = filme.Id, filme });
+    }
+}
