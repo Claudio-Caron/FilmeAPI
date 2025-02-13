e7d5fb70e609665586bb939b363eca17d84305a7
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2025-02-13 16:58:03 -0400

    adicionando endpoints e utilizando AutoMapper para conversão Entidade-DTO

--- FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -1,5 +1,8 @@
-﻿using FilmesAPI.Data;
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.DTO;
 using FilmesAPI.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesAPI.Controllers;
@@ -8,17 +11,25 @@ namespace FilmesAPI.Controllers;
 public class FilmeController:ControllerBase
 {
     private FilmeContext _context;
-
-    public FilmeController(FilmeContext context)
+    private IMapper _mapper;
+    public FilmeController(FilmeContext context, IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperaFilmesTrecho([FromQuery]int skip ,[FromQuery]int take)
+    public IEnumerable<Filme> RecuperaFilmes()
     {
-        return _context.Filmes.Skip(skip).Take(take);
+        return _context.Filmes.ToList();
     }
+
+    //[HttpGet]
+    //public IEnumerable<Filme> RecuperaFilmesTrecho([FromQuery]int skip ,[FromQuery]int take)
+    //{
+    //    return _context.Filmes.Skip(skip).Take(take);
+    //}
+
     [HttpGet("{id}")]
     public IActionResult RecuperaPorId(int id)
     {
@@ -27,10 +38,42 @@ public class FilmeController:ControllerBase
         return Ok(filmesR);
     }
     [HttpPost]
-    public IActionResult AdicionaFilme([FromBody] Filme filme)
+    public IActionResult AdicionaFilme(
+        [FromBody] CreateFilmeDto filmeDto)
     {
+        Filme filme = _mapper.Map<Filme>(filmeDto);
         _context.Filmes.Add(filme);
         _context.SaveChanges();
-        return CreatedAtAction(nameof(RecuperaPorId), new { id = filme.Id, filme });
+        return CreatedAtAction(nameof(RecuperaPorId), new { id = filme.Id }, filme );
+    }
+    [HttpPut("{id}")]
+    public IActionResult AtualizaFilme(int id, 
+        [FromBody] UpdateFilmeDto filmeDto)
+    {
+        var filme = _context.Filmes.FirstOrDefault(x=>x.Id == id);
+        if (filme is null) return NotFound();
+        var filmeEntity = _mapper.Map(filmeDto, filme);
+        _context.Update(filmeEntity);
+        _context.SaveChanges();
+        return NoContent();   
+    }
+    [HttpPatch("{id}")]
+    public IActionResult AtualizaFilmePartial(int id,
+        JsonPatchDocument<UpdateFilmeDto> patch)
+    {
+        var filme = _context.Filmes.FirstOrDefault(x => x.Id == id);
+        if (filme is null) return NotFound();
+
+        var filmeParaAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+
+        patch.ApplyTo(filmeParaAtualizar, ModelState);
+        if (!TryValidateModel(filmeParaAtualizar))
+        {
+            return ValidationProblem(ModelState);
+        }
+        _mapper.Map(filmeParaAtualizar, filme);
+        _context.SaveChanges();
+        return NoContent();
+
     }
 }
---
+++ FilmesAPI/FilmesAPI/Data/DTO/UpdateFilmeDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.DTO;
+
+public class UpdateFilmeDto
+{
+
+    [Required(ErrorMessage = "O titulo do filme eh obrigatorio")]
+    public string? Titulo { get; set; }
+
+    [Required(ErrorMessage = "O genero do filme eh obrigatorio")]
+    [StringLength(50, ErrorMessage = "Genero deve ter menos de 50 caracteres")]
+    public string? Genero { get; set; }
+
+    [Required(ErrorMessage = "A duracao do filme eh obrigatorio")]
+    [Range(70, 600, ErrorMessage = "A duracao deve ser entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
